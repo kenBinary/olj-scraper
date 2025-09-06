cf2052ff7ca168068e4387ffa47c634aede7dcbf
kjbdev <joshuabecaro@gmail.com>
2025-09-06 14:40:24 +0800

    feat: implement job detail scraper functionality

---
+++ scraper/job_detail_scraper.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+import requests
+from bs4 import BeautifulSoup
+import config.urls
+from db.models.Job import Job
+import parser.parsers as parsers
+from services.logger.logger_config import Logger
+
+
+def scrape_job_detail(job_id, logger: Logger) -> Job:
+    logger.info(f"Scraping job detail for Job ID: {job_id}")
+    url = config.urls.BASE_JOB_DETAIL_URL + str(job_id)
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
+        title = parsers.get_title(soup)
+
+        work_type = parsers.get_work_type(soup)
+
+        salary = parsers.get_salary(soup)
+        hours_per_week = parsers.get_hours_per_week(soup)
+        job_overview = parsers.get_job_overview(soup)
+
+        job = Job(
+            job_id=job_id,
+            title=title,
+            work_type=work_type,
+            salary=salary,
+            hours_per_week=hours_per_week,
+            job_overview=job_overview,
+            raw_text=response.text,
+            link=url,
+            date_created=datetime.now().isoformat(),
+        )
+        return job
+    else:
+        logger.error(
+            f"Failed to retrieve job details for Job ID {job_id}. Status code: {response.status_code}"
+        )
