f8c21f30db7f3ff79128b5770d4cb7da09d7938e
kjbdev <joshuabecaro@gmail.com>
2025-09-06 14:40:01 +0800

    feat: refactor engine initialization functions for local and remote setups

--- db/engine/engine.py
+++ db/engine/engine.py
@@ -1,5 +1,21 @@
 from sqlalchemy import create_engine
+import os
+from dotenv import load_dotenv
 
 
-def engine_init():
+def engine_init_local():
     return create_engine("sqlite:///data/olj-scraper.db")
+
+
+def engine_init_remote():
+    load_dotenv()
+    TURSO_DATABASE_URL = os.environ.get("TURSO_DATABASE_URL")
+    TURSO_AUTH_TOKEN = os.environ.get("TURSO_AUTH_TOKEN")
+
+    engine = create_engine(
+        f"sqlite+{TURSO_DATABASE_URL}?secure=true",
+        connect_args={
+            "auth_token": TURSO_AUTH_TOKEN,
+        },
+    )
+    return engine
