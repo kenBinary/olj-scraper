6ec6c230723125e5ac9daa281c93af41000a44bd
kjbdev <joshuabecaro@gmail.com>
2025-09-06 14:43:56 +0800

    feat: add test mode argument to CLI for limited job scraping

--- utils/args_init.py
+++ utils/args_init.py
@@ -5,6 +5,9 @@ def init_cli_args():
     parser = argparse.ArgumentParser(description="OLJ Web Scraper")
     parser.add_argument("--dev", action="store_true", help="Run in development mode")
     parser.add_argument("--prod", action="store_true", help="Run in production mode")
+    parser.add_argument(
+        "--test", action="store_true", help="Run in test mode (scrape only 3 jobs)"
+    )
 
     args = parser.parse_args()
 
